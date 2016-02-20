adb4dc96c9519f7c5345ffbc90f36828fbd23446
Rafael Minguet García <rminguet@gmail.com>
2016-02-20 14:07:43 +0100

    Client Id for GitHub API

--- assets/js/config/dataConfig_dist.js
+++ assets/js/config/dataConfig_dist.js
@@ -1,6 +1,6 @@
 var dataConfig = {
   'host': 'https://api.github.com',
   'server': 'https://github.com',
-  'clientId': '63a2302a9f4c478bcecc',
+  'clientId': '<your_client_id_for github_api>',
   'secretKey': '<your_secret_key_for github_api>'
 };
