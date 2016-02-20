29bac002a7b91c0d9ea8765cb0a98105bfcc4651
Rafael Minguet García <rminguet@gmail.com>
2016-02-20 14:05:37 +0100

    Added secret key for GitHub API

--- assets/js/config/dataConfig_dist.js
+++ assets/js/config/dataConfig_dist.js
@@ -1,5 +1,6 @@
 var dataConfig = {
   'host': 'https://api.github.com',
   'server': 'https://github.com',
-  'clientId': '63a2302a9f4c478bcecc'
+  'clientId': '63a2302a9f4c478bcecc',
+  'secretKey': '<your_secret_key_for github_api>'
 };
