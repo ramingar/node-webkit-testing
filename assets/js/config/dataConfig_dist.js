1bbefb0a2ea3d3af0ad1ba570b30ec33a9318f08
Rafael Minguet García <rminguet@gmail.com>
2016-02-21 03:19:50 +0100

    Due to explicit grant type OAuth2 from GitHub API authorization, secret key is not yet neccessary at client side application

--- assets/js/config/dataConfig_dist.js
+++ assets/js/config/dataConfig_dist.js
@@ -1,6 +1,5 @@
 var dataConfig = {
   'host': 'https://api.github.com',
   'server': 'https://github.com',
-  'clientId': '<your_client_id_for github_api>',
-  'secretKey': '<your_secret_key_for github_api>'
+  'clientId': '<your_client_id_for github_api>'
 };
